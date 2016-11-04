11a3d9c5b786e093943eed9a528e7383d6d9a76a
Radoslaw Chmielewski <tatry@interia.pl>
2016-11-04 08:46:05 +0100

    Folder changed again

---
+++ HC.Radek/HC.Radek.Ex1/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    public abstract class Animal
+    {
+        protected string name;
+        protected string species;
+        protected string sound;
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getSpecies()
+        {
+            return species;
+        }
+
+        public string getSound()
+        {
+            return sound;
+        }
+
+    }
+}
---
+++ HC.Radek/HC.Radek.Ex1/AnimalFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    class AnimalFactory
+    {
+        public Animal getAnimal(String species, String name)
+        {
+            switch (species)
+            {
+                case "Cat":
+                    return new Cat(name);
+                case "Dog":
+                    return new Dog(name);
+                case "Sheep":
+                    return new Sheep(name);
+                default:
+                    return new Cow(name);
+            }
+        }
+    }
+}
