58d6386b65adfbe623860419727987412de8613c
Kamil Wołczyk <k.wolczyk@headchannel.co.uk>
2016-11-14 22:12:09 +0100

    CsvUserRepository

---
+++ Hc.Kamil/Hc.Kamil.Ex3.RegistrationForm/Repositories/CsvFileUserRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Hc.Kamil.Ex3.RegistrationForm.Models;
+using Hc.Kamil.Ex3.RegistrationForm.Security;
+
+namespace Hc.Kamil.Ex3.RegistrationForm.Repositories
+{
+    public class CsvFileUserRepository : IUserRepository
+    {
+        public const char CsvSeparator = ',';
+
+        private readonly string _fileName;
+
+        private readonly IHashProvider _hashProvider;
+
+        private IList<User> _users;
+
+        public CsvFileUserRepository(string fileName, IHashProvider hashProvider)
+        {
+            _fileName = fileName;
+            _hashProvider = hashProvider;
+        }
+
+        private IList<User> Users
+        {
+            get { return _users ?? (_users = ReadUsersFromFile()); }
+        }
+
+        public User GetUser(string login, string password)
+        {
+            var hash = _hashProvider.GetHash(password);
+            return Users.FirstOrDefault(user => user.Login == login && user.Password == hash);
+        }
+
+        public bool ExistUser(string login)
+        {
+            return Users.Any(user => user.Login == login);
+        }
+
+        public void AddUser(User user)
+        {
+            if (ExistUser(user.Login))
+                throw new InvalidOperationException($"User with login {user.Login} currently exists.");
+
+            user.Password = _hashProvider.GetHash(user.Password);
+
+            Users.Add(user);
+            WriteUserToEndOfFile(user);
+        }
+
+        private void EnsureFileExists()
+        {
+            if (File.Exists(_fileName))
+                return;
+
+            var file = File.Create(_fileName);
+            file.Dispose();
+        }
+
+        private IList<User> ReadUsersFromFile()
+        {
+            EnsureFileExists();
+            return File.ReadAllLines(_fileName)
+                       .Select(line =>
+                       {
+                           var parts = line.Split(CsvSeparator);
+                           return parts.Count() == 2 ? new User(parts[0], parts[1]) : null;
+                       })
+                       .Where(item => item != null)
+                       .ToList();
+        }
+
+        private void WriteUserToEndOfFile(User user)
+        {
+            EnsureFileExists();
+            using (var file = File.AppendText(_fileName))
+                file.WriteLine($"{user.Login}{CsvSeparator}{user.Password}");
+        }
+    }
+}
---
+++ Hc.Kamil/Hc.Kamil.Ex3.RegistrationForm/Security/Md5HashProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Hc.Kamil.Ex3.RegistrationForm.Security
+{
+    public class Md5HashProvider : IHashProvider
+    {
+        public string GetHash(string value)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider();
+            byte[] bytes = provider.ComputeHash(new UTF8Encoding().GetBytes(value));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+    }
+}
