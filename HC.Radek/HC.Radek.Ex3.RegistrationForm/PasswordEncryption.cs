f710f416514c1dedd24f662c6b41581435af669b
Radoslaw Chmielewski <tatry@interia.pl>
2016-11-16 15:47:18 +0100

    .

--- HC.Radek/HC.Radek.Ex3.RegistrationForm/PasswordEncryption.cs
+++ HC.Radek/HC.Radek.Ex3.RegistrationForm/PasswordEncryption.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace HC.Radek.Ex3.RegistrationForm
 {
