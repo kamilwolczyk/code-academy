702ab46ccf2833166d73e68a8f870a2c9510c370
tomeklewus <tomeklewus@gmail.com>
2016-12-18 21:01:10 +0100

    mvc-routing

---
+++ MVC.Tomek/Ex0.Routing/Models/DateTimeModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Ex0.Routing.Models
+{
+    public class DateTimeModel
+    {
+        public string GenerateDate()
+        {
+            string _date="";
+            DateTime _localDate = DateTime.Now;
+            _date = _localDate.ToString("yyyy-MM-dd");
+            return _date;
+        }
+
+        public string GenerateTime()
+        {
+            string _time = "";
+            DateTime _localTime = DateTime.Now;
+            _time = _localTime.ToString("HH:mm");
+            return _time;
+        }
+
+        //public string GenerateTime(string _format)
+        //{
+        //    string _time = "";
+        //    DateTime _localTime = DateTime.Now;
+        //    _time = _localTime.ToString(_format);
+        //    return _time;
+        //}
+    }
+}
