37d92d1c7467b06bed44388f096d886d3b448850
tomeklewus <tomeklewus@gmail.com>
2017-01-31 23:04:01 +0100

    css

---
+++ HC.Tomek/HC.Tomek.Ex6.Forms/Scripts/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    //$.ajax({
+    //    url: "Ajax/HelloWorld",
+    //    success: function (result) {
+    //        alert(result);
+    //    }
+    //})
+    $.getJSON("HelloWorld", function (result) {
+        alert(result.FirstName+" "+result.Surname);
+    });
+})
+
