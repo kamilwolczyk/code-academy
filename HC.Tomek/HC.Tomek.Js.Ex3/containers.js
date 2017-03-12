291672b93650e7914eb8c3b5bc23797154526fed
tomeklewus <tomeklewus@gmail.com>
2017-03-12 21:59:52 +0100

    Js.Ex3 done Js.Ex4 done

--- HC.Tomek/HC.Tomek.Js.Ex3/containers.js
+++ HC.Tomek/HC.Tomek.Js.Ex3/containers.js
@@ -17,17 +17,26 @@ function quizPage() {
     var quizContainer = document.getElementById("quiz");
     var questionNumber = 3;
     var maximumAnswers = 4;
+    var correctAnswers = 0;
     var currentQuestionNumber = 0;
     var questionText = new Array(questionNumber);
     
-    var questionAnswerRadio = new Array(questionNumber, maximumAnswers);
-    var questionAnswerText = new Array(questionNumber, maximumAnswers);
-    var answerControl = new Array(questionNumber, maximumAnswers);
-    var questionControl = new Array(questionNumber);
-
+    var questionAnswerRadio = new Array(questionNumber);
+    var questionAnswerText = new Array(questionNumber);
+    var answerControl = new Array(questionNumber);
+    for (var z = 0; z < maximumAnswers; z++) {
+        questionAnswerRadio[z] = new Array(maximumAnswers);
+        questionAnswerText[z]= new Array(maximumAnswers);
+        answerControl[z] = new Array(maximumAnswers);
+    }
+    
+    var questionStatus = document.createElement("label");
+    questionStatus.innerHTML = (currentQuestionNumber + 1) + " / " + questionNumber + " ";
+    quizContainer.appendChild(questionStatus);
     var nextButton = document.createElement("button");
     nextButton.innerHTML = "Next";
     quizContainer.appendChild(nextButton);
+    
 
     questionaire.generateAsync(questionNumber, function (questions) {
         for (var j = 0; j < questionNumber; j++) {
@@ -40,46 +49,74 @@ function quizPage() {
             }
             quizContainer.appendChild(questionText[j]);
             for (var i = 0; i < answerNumber; i++) {
-                answerControl[j,i] = document.createElement("p");
-                questionAnswerRadio[j,i] = document.createElement("input");
-                questionAnswerRadio[j,i].setAttribute("type", "radio");
-                questionAnswerText[j,i] = document.createElement("label");
-                questionAnswerText[j,i].innerHTML = questions[j].answears[i].text;
-                answerControl[j,i].appendChild(questionAnswerRadio[j,i]);
-                answerControl[j,i].appendChild(questionAnswerText[j,i]);
+                answerControl[j][i] = document.createElement("p");
+                questionAnswerRadio[j][i] = document.createElement("input");
+                questionAnswerRadio[j][i].setAttribute("type", "radio");
+                questionAnswerRadio[j][i].name = "answer";
+                questionAnswerRadio[j][i].onchange = onRadioClick(j,i);
+                questionAnswerText[j][i] = document.createElement("label");
+                questionAnswerText[j][i].innerHTML = questions[j].answears[i].text;
+                answerControl[j][i].appendChild(questionAnswerRadio[j][i]);
+                answerControl[j][i].appendChild(questionAnswerText[j][i]);
                 if (j>0) {
-                    answerControl[j,i].style.display = "none";
+                    answerControl[j][i].style.display = "none";
                 }
-                quizContainer.appendChild(answerControl[j,i]);
+                quizContainer.appendChild(answerControl[j][i]);
 
             }
-            
-       
 
         }
         nextButton.addEventListener("click", function () {
             
-            for (var k = 0; k < questionNumber; k++) {
-                if (k == currentQuestionNumber) {
-                    questionText[k].style.display = "";
-                    for (var g = 0; g < questions[k].answears.length; g++) {
-                        answerControl[k,g].style.display = "";
+            
+            if (currentQuestionNumber >= questionNumber-1) {
+                nextButton.style.display = "none";
+                questionStatus.style.display = "none";
+                questionText[currentQuestionNumber].style.display = "none";
+                for (var c = 0; c < questions[currentQuestionNumber].answears.length; c++) {
+                    answerControl[currentQuestionNumber][c].style.display = "none";
+                    if ((questionAnswerRadio[currentQuestionNumber][c].checked == true) &&
+                        (questions[currentQuestionNumber].answears[c].isCorrect == true)) {
+                        correctAnswers++;
                     }
                 }
-                else {
-                    questionText[k].style.display = "none";
-                    for (var c = 0; c < questions[k].answears.length; c++) {
-                        answerControl[k,c].style.display = "none";
-                    }
-
-                }
-                currentQuestionNumber++;
+                
+                resultPage(correctAnswers, questionNumber);
             }
+            else {
+                questionText[currentQuestionNumber].style.display = "none";
+                for (var c = 0; c < questions[currentQuestionNumber].answears.length; c++) {
+                    answerControl[currentQuestionNumber][c].style.display = "none";
+                    if ((questionAnswerRadio[currentQuestionNumber][c].checked == true) &&
+                        (questions[currentQuestionNumber].answears[c].isCorrect == true)) {
+                        correctAnswers++;
+                    }
+                    }
+                
+                questionText[currentQuestionNumber + 1].style.display = "";
+                for (var g = 0; g < questions[currentQuestionNumber + 1].answears.length; g++) {
+                    answerControl[currentQuestionNumber + 1][g].style.display = "";
+                }
+                if (currentQuestionNumber >= questionNumber - 2) nextButton.innerHTML = "End";
 
+            }
+            currentQuestionNumber++;
+            questionStatus.innerHTML = (currentQuestionNumber + 1) + " / " + questionNumber + " ";
+            
         });
 
         });
     
 }
+
+function resultPage(correctAnswers,questionNumber) {
+    var result = document.getElementById("result");
+    result.style.display = "";
+    var wynik = document.createElement("label");
+    wynik.innerHTML = "Wynik: " + correctAnswers +"/" + questionNumber;
+    result.appendChild(wynik);
+    
+}
+
 
 
