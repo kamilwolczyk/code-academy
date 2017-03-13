60eab07fea21b52e252a981fec672e27ead1f99d
oshiinka <maggie@headchannel.co.uk>
2017-03-13 22:28:14 +0100

    poprawka colorPicker js5

---
+++ Hc.Gosia/Hc.Gosia.Js.Ex5/colorPicker.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $("div").click(function (event) {
+        var colorPicker = event.target;
+        var selectedColor = $("#color");
+        var selectedColorText = $("span");
+        selectedColor.removeClass();
+        selectedColorText.empty();
+        selectedColor.addClass(colorPicker.className);
+        var color = $(event.target).attr("title");
+        selectedColorText.append(color);
+    });
+})
